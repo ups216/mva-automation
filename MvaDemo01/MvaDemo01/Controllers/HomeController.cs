188cbc6f86ab3f2defb6054921ef57f8afe4941d
Lei Xu <leixu216@gmail.com>
2015-05-12 09:04:59 +0800

    mva demo asp.net application with built-in exception on about us page

---
+++ MvaDemo01/MvaDemo01/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvaDemo01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.MyMessage = GetAboutUsContent(0);
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        private string GetAboutUsContent(int num)
+        {
+            string message = string.Empty;
+            for (int i = 0; i < 100; i++)
+            {
+                message += "<br/> Test line " + i/num;
+            }
+
+            return message;
+        }
+    }
+}
---
+++ MvaDemo01/MvaDemo01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvaDemo01.MvcApplication" Language="C#" %>
