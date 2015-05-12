c7391dc8fdfa25bd545853477a9a77ff6037e02d
Lei Xu <leixu216@gmail.com>
2015-05-12 09:37:52 +0800

    added demo script

---
+++ scripts/demo-script.ps1
@@ -0,0 +1,4 @@
+﻿Add-AzureAccount -Environment AzureChinaCloud
+Start-AzureAutomationRunbook -AutomationAccountName 'lxdemoaccount01' -Name 'MVADemo01-StartTrace'
+Start-AzureAutomationRunbook -AutomationAccountName 'lxdemoaccount01' -Name 'MVADemo01-StopTrace'
+Start-AzureAutomationRunbook -AutomationAccountName 'lxdemoaccount01' -Name 'MVADemo01-UploadLogToBlob'
